5ea4aff12f06d8b554f55e6da7fe7b17a37fb65a
AdomusP-0 <igorkudla0@gmail.com>
2024-11-25 13:36:57 +0200

    Update urok222.py

--- urok2/urok222.py
+++ urok2/urok222.py
@@ -1,18 +1,18 @@
 list_append = [1, 2, 3]
-print(list_append)
+print(list_append)           #Пише list_append
 
 list_append.append(4)
-print(list_append)
+print(list_append)            #list_append поширюєтся до 4 цифр
 
-list_append.append(5)
+list_append.append(5)          #list_append поширюєтся до 5 цифр
 print(list_append)
 
 list_extend = [4, 5, 6]
+print(list_extend)              #Створюється list_extend 
+list_extend.extend([7, 8, 9])    #list_extend поширюєтся символами 7, 8, 9
 print(list_extend)
-list_extend.extend([7, 8, 9])
-print(list_extend)
 
 
-print(list_extend.index(6))
+print(list_extend.index(6))        #Видає індекс 6 у list_extend
 
-print(len(list_append))
+print(len(list_append))            #Видає довжину list_extend
